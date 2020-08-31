aa8fdacd7d9519ca6bc20abf3d25223a241df6ac
taitus <sebastia.roig@gmail.com>
2020-08-31 13:28:54 +0200

    Remove deprecated attribute from Comments
    A field from Comment is deprecated and we no longer need it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200831112342) do
+ActiveRecord::Schema.define(version: 20200831112559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -397,7 +397,6 @@ ActiveRecord::Schema.define(version: 20200831112342) do
   create_table "comments", id: :serial, force: :cascade do |t|
     t.integer "commentable_id"
     t.string "commentable_type"
-    t.text "deprecated_body"
     t.string "subject"
     t.integer "user_id", null: false
     t.datetime "created_at"
